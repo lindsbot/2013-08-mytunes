2f235d4c29e12cb09b3a8ddf780ea24068bbe437
Hack Reactor Students <github@hackreactor.com>
2013-08-21 21:00:09 -0700

    Click queue entry to dequeue now working

--- src/SongQueueView.js
+++ src/SongQueueView.js
@@ -11,6 +11,7 @@ var SongQueueView = Backbone.View.extend({
     }, this);
   },
 
+
   render: function(){
     // to preserve event handlers on child nodes, we must call .detach() on them before overwriting with .html()
     // see http://api.jquery.com/detach/
