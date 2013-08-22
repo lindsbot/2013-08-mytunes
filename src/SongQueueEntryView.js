c6d8ac8252b793b89903dfa0b705ee49888d441d
Hack Reactor Students <github@hackreactor.com>
2013-08-21 20:27:17 -0700

    SongQueueViewSpec passing all tests

--- src/SongQueueEntryView.js
+++ src/SongQueueEntryView.js
@@ -1,3 +1,9 @@
 var SongQueueEntryView = Backbone.View.extend({
 
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
+
+  render: function() {
+    return this.$el.html(this.template(this.model.attributes));
+  }
+
 });
--- src/SongQueueView.js
+++ src/SongQueueView.js
@@ -1,15 +1,25 @@
 var SongQueueView = Backbone.View.extend({
 
+  tagName: "table",
+
   initialize: function() {
-    this.model.on('add', function(){
-      if(this.model.list.length === 1){
-        this.model.list[0].play();
-      }
-    });
+    this.collection.on('add', function(){
+      this.render();
+    }, this);
+    this.collection.on('remove', function(){
+      this.render();
+    }, this);
   },
 
-  render: function() {
-    return this.$el;
-  }
+  render: function(){
+    // to preserve event handlers on child nodes, we must call .detach() on them before overwriting with .html()
+    // see http://api.jquery.com/detach/
+    this.$el.children().detach();
 
+    return this.$el.append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
+  }
 });
